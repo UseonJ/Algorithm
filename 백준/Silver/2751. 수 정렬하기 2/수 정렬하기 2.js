21493005b90c0226afe515ff29fa1ab0c44f4c2c
UseonJ <114300611+UseonJ@users.noreply.github.com>
2023-07-25 09:20:34 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1428 ms, Memory: 215112 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,19 @@
+let fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let n = Number(input[0]);
+
+let arr = [];
+for(let i=1; i<=n; i++){
+    arr.push(Number(input[i]))
+}
+
+let sorted = arr.sort((a,b) => a-b)
+
+let answer = "";
+
+for (let i=0; i<sorted.length; i++){
+    answer += sorted[i] +"\n"
+}
+
+console.log(answer)
