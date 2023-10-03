3b6dd0008ba80a6c0cf616db47b8afd0b1cfbdea
UseonJ <114300611+UseonJ@users.noreply.github.com>
2023-10-03 21:10:55 +0900

    [unrated] Title: 길이에 따른 연산, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -0,0 +1,12 @@
+function solution(num_list) {
+    if(num_list.length >= 11) {
+        return num_list.reduce((a,c) => {
+            return c = c+a
+        })
+    } else {
+        return num_list.reduce((a,c) => {
+            return c = c*a
+        },1)
+    }
+        
+}
