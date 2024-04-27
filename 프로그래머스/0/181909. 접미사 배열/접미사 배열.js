807274426956c65085e06817e9bd6cfa75819f65
UseonJ <114300611+UseonJ@users.noreply.github.com>
2024-04-28 01:58:06 +0900

    [level 0] Title: 접미사 배열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -5,3 +5,10 @@ function solution(my_string) {
     }
     return answer.sort();
 }
+
+// function solution(my_string) {
+//   return Array.from(my_string)
+//     .map((_, i) => my_string.substring(i))
+//     .sort();
+// }
+
