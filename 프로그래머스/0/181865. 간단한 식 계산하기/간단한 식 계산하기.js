f2dfb572d043f7f6dc3c032c422097823df6ce0d
UseonJ <114300611+UseonJ@users.noreply.github.com>
2024-04-27 23:51:47 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -2,3 +2,14 @@ function solution(binomial) {
     const array = binomial.split(' ');
     return array[1] === '+'? Number(array[0]) + Number(array[2]) : array[1] === '-'? Number(array[0]) - Number(array[2]) : Number(array[0]) * Number(array[2]) 
 }
+
+// const ops = {
+//   '+': (a, b) => a + b,
+//   '-': (a, b) => a - b,
+//   '*': (a, b) => a * b,
+// };
+
+// function solution(binomial) {
+//   const [a, op, b] = binomial.split(' ');
+//   return ops[op](+a, +b);
+// }
