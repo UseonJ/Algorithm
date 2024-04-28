68303393edba8e458b061c9e3c0883e01a03a977
UseonJ <114300611+UseonJ@users.noreply.github.com>
2024-04-28 15:19:58 +0900

    [level 0] Title: 세로 읽기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -3,6 +3,10 @@
 //     return ;
 // }
 
+// function solution(my_string, m, c) {
+//     return [...my_string].filter((_, i) => i % m === c - 1).join('');
+// }
+
 function solution(my_string, m, c) {
     let result = '';
 
