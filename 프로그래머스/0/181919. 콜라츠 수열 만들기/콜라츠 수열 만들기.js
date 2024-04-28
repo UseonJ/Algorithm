de06ee3e265ece292dabc4c5d485f0a6ea58aebf
UseonJ <114300611+UseonJ@users.noreply.github.com>
2024-04-28 10:05:50 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -12,3 +12,10 @@ function solution(n) {
     }
     return answer;
 }
+
+// function solution(n, arr = []) {
+//     arr.push(n)
+//     if (n === 1) return arr
+//     if (n % 2 === 0) return solution(n / 2, arr)
+//     return solution(3 * n + 1, arr)
+// }
