3e89930c9b652e6fea27852538e8954f8f4d7252
UseonJ <114300611+UseonJ@users.noreply.github.com>
2024-05-03 23:28:28 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.js
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.js
@@ -3,3 +3,10 @@ function solution(arr, flag) {
     flag.map((a,i) => a? answer.push(...Array.from({length: arr[i] * 2}, () => arr[i])) : answer.splice(-arr[i], arr[i]));
     return answer;
 }
+
+// function solution(arr, flag) {
+//   return arr.reduce(
+//     (prev, num, i) => (flag[i] ? [...prev, ...new Array(num * 2).fill(num)] : prev.slice(0, -num)),
+//     [],
+//   );
+// }
