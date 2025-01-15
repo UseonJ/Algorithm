8d7d3b457c5dbec817c16ccf33959b31c96200ec
UseonJ <114300611+UseonJ@users.noreply.github.com>
2025-01-15 15:19:28 +0900

    [level 1] Title: 직사각형 별찍기, Time: 69.42 ms, Memory: 32.1 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -7,3 +7,10 @@ process.stdin.on('data', data => {
         console.log(line)
     }
 });
+
+// repeat라는게 있음
+//     const n = data.split(" ");
+//     const a = Number(n[0]), b = Number(n[1]);
+//     const row = '*'.repeat(a)
+//     for(let i =0; i < b; i++){
+//         console.log(row)
