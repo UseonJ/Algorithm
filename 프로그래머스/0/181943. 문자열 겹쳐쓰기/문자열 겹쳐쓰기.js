8a5b28ee8a6ca2589372d07efef19b4de152d8b8
UseonJ <114300611+UseonJ@users.noreply.github.com>
2025-01-15 16:18:28 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -4,3 +4,6 @@ function solution(my_string, overwrite_string, s) {
     myarr.splice(s,overarr.length,...overarr)
     return myarr.join('')
 }
+
+// string도 slice 가능
+// return my_string.slice(0,s)+overwrite_string+my_string.slice(s+overwrite_string.length);
