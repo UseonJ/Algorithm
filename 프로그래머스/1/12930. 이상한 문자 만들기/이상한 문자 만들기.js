7a23ab1899e7ce3ed1e0aea4a6e737f01f28f1d9
UseonJ <114300611+UseonJ@users.noreply.github.com>
2025-01-18 22:03:38 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,11 @@
+function solution(s) {
+    var answer = '';
+    let arr = s.split(' ')
+    for(let i=0; i<arr.length; i++) {
+        for(let j=0; j<arr[i].length; j++){
+            j%2===0? answer += arr[i][j].toUpperCase() : answer += arr[i][j].toLowerCase()    
+        }
+        if(i !== (arr.length-1)) answer += ' '
+    }
+    return answer;
+}
