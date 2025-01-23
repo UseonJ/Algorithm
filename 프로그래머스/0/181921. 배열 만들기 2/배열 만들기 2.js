5b02e04d2a7db64a0351bc49b76bc231dc1662e2
UseonJ <114300611+UseonJ@users.noreply.github.com>
2025-01-23 23:12:35 +0900

    [level 0] Title: 배열 만들기 2, Time: 112.72 ms, Memory: 52.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -0,0 +1,27 @@
+function solution(l, r) {
+    let answer = [];
+    for (let i=l; i <= r; i++) {
+        if ((/^[05]+$/).test(String(i))) answer.push(i)
+    }
+    return answer.length === 0? [-1] : answer
+}
+
+// // 숫자 5로 현혹시켰지만 사실 이건 이진수 문제임.
+// function* gen50() {
+//     let i = 1;
+
+//     while(true) {
+//         yield Number(Number(i).toString(2)) * 5;
+//         i++;
+//     }
+// }
+// function solution(l, r) {
+//     const n = gen50();
+//     let a = 0;
+//     const arr = [];
+
+//     while(a < l) { a = n.next().value; }
+//     while(a <= r) { arr.push(a); a = n.next().value; }
+
+//     return arr.length ? arr : [-1];
+// }
