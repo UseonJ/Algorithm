829b45e79fbeafe290216e77fddaa1dda91d441d
UseonJ <114300611+UseonJ@users.noreply.github.com>
2025-01-25 23:32:46 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -1,9 +1,10 @@
 function solution(sides) {
-    let sideSet = new Set()
     let diff = Math.abs(sides[0]-sides[1])
     let sum = Math.abs(sides[0]+sides[1])
-    for(let i = diff+1; i<sum; i++) {
-        
-    }
+    return Math.min(...sides)*2 - 1
     return sum - (diff+1)
+    
+    // 큰거 + 작은거 - ((큰거 - 작은거)+1)
+    // 작은거 두 개 - 1
+    // Math.min(...sides)*2 - 1
 }
