25ea061d617329e2c0c868e2f39270ce0d887c9d
UseonJ <114300611+UseonJ@users.noreply.github.com>
2025-02-07 23:48:32 +0900

    [level 1] Title: 카드 뭉치, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -10,3 +10,15 @@ function solution(cards1, cards2, goal) {
     }
     return "Yes"
 }
+
+// O(n) 으로 줄이기
+// function solution(cards1, cards2, goal) {
+//     let j = 0;
+//     let k = 0;
+//     for(let i=0;i<goal.length;i++){
+//         if(goal[i] == cards1[j]) j++;
+//         else if(goal[i] == cards2[k]) k++;
+//         else return "No"
+//     }
+//     return "Yes";
+// }
