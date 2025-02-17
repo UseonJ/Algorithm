a8f030e12bb008bc449b1ab1f224f83dcc9d6d32
UseonJ <114300611+UseonJ@users.noreply.github.com>
2025-02-17 20:29:58 +0900

    [level 2] Title: 오픈채팅방, Time: 253.94 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -17,3 +17,29 @@ function solution(record) {
     })
     return answer;
 }
+
+// 내 힘으로 매핑 깔끔하게 해서 기분좋다. 더 효율적일 수도 있는 다른 풀이들 검토.
+// function solution(record) {
+//     const userInfo = {};
+//     const action = [];
+//     const stateMapping = {
+//         'Enter': '님이 들어왔습니다.',
+//         'Leave': '님이 나갔습니다.'
+//     }
+
+//     record.forEach((v) => {
+//         const [state, id, nick] = v.split(' ');
+
+//         if(state !== "Change") {
+//             action.push([state, id]);
+//         }
+
+//         if(nick) {
+//             userInfo[id] = nick;
+//         }
+//     })
+
+//     return action.map(([state, uid]) => {
+//         return `${userInfo[uid]}${stateMapping[state]}`;    
+//     })
+// }
